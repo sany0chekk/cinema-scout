70ee13b3f0e344dfa0be1ccae39c0888155d1699
Oleksandr <olexandr.veseluy@gmail.com>
2024-05-09 22:50:45 +0300

    v0.0.5

--- src/js/render-list.js
+++ src/js/render-list.js
@@ -7,7 +7,12 @@ renderPopularMovies();
 let currentPage = 1;
 
 const searchForm = document.querySelector('.header-search');
-searchForm.addEventListener('submit', renderElements);
+searchForm.addEventListener('submit', event => {
+  const pageCounterEl = document.querySelector('.library-page');
+  currentPage = 1;
+  pageCounterEl.textContent = currentPage;
+  renderElements(event);
+});
 
 const prevPageBtn = document.querySelector('.library-prev-btn');
 const nextPageBtn = document.querySelector('.library-next-btn');
@@ -37,6 +42,25 @@ function handleNextPage() {
     top: 0,
     behavior: 'smooth',
   });
+}
+
+window.addEventListener('popstate', handlePopstate);
+
+function handlePopstate(event) {
+  const pathname = location.pathname;
+
+  if (pathname === '/popular') {
+    currentPage = 1;
+    renderPopularMovies();
+  } else if (pathname.startsWith('/search/')) {
+    const pathParts = pathname.substring('/search/'.length).split('/');
+    const searchPage = pathParts[0] || 1;
+    const searchQuery = pathParts[1] || '';
+    currentPage = parseInt(searchPage, 10);
+    renderElements({
+      target: { elements: { 'header-search-input': { value: searchQuery } } },
+    });
+  }
 }
 
 async function renderElements(event) {
@@ -77,16 +101,16 @@ async function renderElements(event) {
             }
 
             return `
-        <li class="library-item">
+          <li class="library-item">
           <img src="https://image.tmdb.org/t/p/w500/${img}" alt="${title}" class="library-item-poster" />
           <div class="library-item-content">
-            <h3 class="library-item-title">${title}</h3>
-            <p class="library-item-date library-item-text">Release date: <span>${date}</span></p>
-            <p class="library-item-rate library-item-text">Rating: <span>${rate}</span></p>
-            <button type="button" class="library-item-btn" data-id="${id}">Read more</button>
+          <h3 class="library-item-title">${title}</h3>
+          <p class="library-item-date library-item-text">Release date: <span>${date}</span></p>
+          <p class="library-item-rate library-item-text">Rating: <span>${rate}</span></p>
+          <button type="button" class="library-item-btn" data-id="${id}">Read more</button>
           </div>
-        </li>
-      `;
+          </li>
+          `;
           }
         )
         .join('');
@@ -107,6 +131,16 @@ async function renderElements(event) {
         prevPageBtn.disabled = false;
       }
     }
+
+    if (movieName.value.trim()) {
+      history.pushState(
+        {},
+        '',
+        `/search/${movieName.value.trim()}/${currentPage}`
+      );
+    } else {
+      history.pushState({}, '', '/popular');
+    }
   } catch (error) {
     console.log(error);
   } finally {
@@ -117,6 +151,8 @@ async function renderElements(event) {
 async function renderPopularMovies() {
   const libraryEl = document.querySelector('.library-list');
   const loader = document.querySelector('.library-loader');
+
+  history.pushState({}, '', '/popular');
 
   libraryEl.innerHTML = '';
   loader.classList.remove('hide');
